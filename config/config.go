4bb0f8b3600849db7fbce33bf3beeca2a9c1e691
ilya <uumkon@gmail.com>
2024-11-23 11:56:39 +0500

    add backend repo

--- config/config.go
+++ config/config.go
@@ -1,6 +1,14 @@
 package config
 
+import (
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+)
+
 type Config struct {
+	ServerPort    string
 	Neo4jURI      string
 	Neo4jUsername string
 	Neo4jPassword string
@@ -8,10 +16,26 @@ type Config struct {
 }
 
 func LoadConfig() Config {
-	return Config{
-		Neo4jURI:      "bolt://localhost:7687",
-		Neo4jUsername: "neo4j",
-		Neo4jPassword: "securepassword",
-		AuthToken:     "your-secret-token",
+	// Загружаем переменные из .env файла
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatalf("Ошибка загрузки .env файла: %v", err)
 	}
+
+	return Config{
+		ServerPort:    getEnv("SERVER_PORT", "8080"),
+		Neo4jURI:      "bolt://" + getEnv("NEO4J_HOST", "localhost") + ":" + getEnv("NEO4J_BOLT_PORT", "7687"),
+		Neo4jUsername: getEnv("NEO4J_USER", "neo4j"),
+		Neo4jPassword: getEnv("NEO4J_PASSWORD", "securepassword"),
+		AuthToken:     getEnv("AUTH_TOKEN", "your-secret-token"),
+	}
+}
+
+// getEnv возвращает значение переменной среды или значение по умолчанию
+func getEnv(key, defaultValue string) string {
+	value := os.Getenv(key)
+	if value == "" {
+		return defaultValue
+	}
+	return value
 }
